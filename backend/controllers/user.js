d94a6f750da084262ccf91434c4512ded5829712
BenV-V <70845425+BenV-V@users.noreply.github.com>
2021-03-03 07:44:59 +0100

    Add files via upload

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,57 +5,57 @@ require('dotenv').config();
 
 // Création du nouvel utilisateur et cryptage de son mot de passe
 exports.signup = (req, res, next) => {
-  const user = req.body
-  bcrypt.hash(user.password, 10) 
-  .then((hash) => {
-    user.password = hash
-    //Enregistrement dans la base de données
-    db.query(`INSERT INTO users VALUES (NULL, '${req.body.lastname}', '${req.body.firstname}', '${password}', '${email}', 0)`,(error, results, fields) => {
-        if (error) {
-            return res.status(400).json("erreur");
-        }
-        return res.status(201).json({
-            message: 'Votre compte a bien été crée !'
-        });
-    });
-  })
-  .catch(error => res.status(500).json({error}));
-};
-
-//Si utilisateur déjà existant
-exports.login = (req, res, next) => {
-  db.query(`SELECT * FROM users WHERE email='${req.body.email}'`,() => {
-      if (!user) {
-        return res.status(404).json({ message: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-              return res.status(403).json({ message: 'Mot de passe incorrect !' });
+    const user = req.body
+    bcrypt.hash(user.password, 10) 
+    .then((hash) => {
+      user.password = hash
+      //Enregistrement dans la base de données
+      db.query(`INSERT INTO users VALUES (NULL, '${req.body.lastname}', '${req.body.firstname}', '${password}', '${email}', 0)`,(error, results, fields) => {
+          if (error) {
+              return res.status(400).json("erreur");
           }
-          res.status(200).json({
-            userId: user._id,
-            lastname: user.lastname,
-            firstname: user.firstname,
-            admin: user.admin,
-            token: jwt.sign(
-              { userId: user._id },
-              'RANDOM_TOKEN_SECRET',
-              { expiresIn: '24h' }
-            )
+          return res.status(201).json({
+              message: 'Votre compte a bien été crée !'
           });
-        })
-        .catch(error => res.status(500).json({ error }));
-    })    
-    .catch(error => res.status(500).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({error}));
   };
-
-// Suppression de l'utilisateur
-exports.delete = (req, res, next) => {
-  db.query(`DELETE FROM users WHERE users.id = ${req.params.id}`, (error, result, field) => {
-    if (error) {
-        return res.status(400).json({error});
-    }
-    return res.status(200).json(result);
-  });
-};
+  
+  //Si utilisateur déjà existant
+  exports.login = (req, res, next) => {
+    db.query(`SELECT * FROM users WHERE email= ?`, req.body.email,() => {
+        if (!user) {
+          return res.status(404).json({ message: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+                return res.status(403).json({ message: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({
+              user_id: user._id,
+              lastname: user.lastname,
+              firstname: user.firstname,
+              admin: user.admin,
+              token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })    
+      .catch(error => res.status(500).json({ error }));
+    };
+  
+  // Suppression de l'utilisateur
+  exports.delete = (req, res, next) => {
+    db.query(`DELETE FROM users WHERE users.id = ${req.params.id}`, (error, result, field) => {
+      if (error) {
+          return res.status(400).json({error});
+      }
+      return res.status(200).json(result);
+    });
+  };
--- backend/database/groupomaniabase.sql
+++ backend/database/groupomaniabase.sql
@@ -68,7 +68,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(80) NOT NULL,
   `email` varchar(80) NOT NULL,
   `admin` tinyint(2) NOT NULL DEFAULT 0,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 --
